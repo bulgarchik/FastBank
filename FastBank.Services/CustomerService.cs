48c53e0a4de601328221eada79c7c643e74508a1
Alexander Shevrikuko <ashevrikuko@melontech.com>
2023-10-01 00:15:51 +0300

    Add validation for exist customer by name or email

--- FastBank.Services/CustomerService.cs
+++ FastBank.Services/CustomerService.cs
@@ -7,10 +7,12 @@ namespace FastBank.Services
     {
         readonly private ICustomerRepository _customerRepo;
 
-        public CustomerService() 
+        public CustomerService()
         {
             _customerRepo = new CustomerRepository();
         }
+
+        public List<string> ValidationErrors { get; private set; } = new List<string>();
 
         public List<Customer> GetAll()
         {
@@ -25,8 +27,43 @@ namespace FastBank.Services
         public void Add(string name, string email, DateTime birthday, string password, Roles role)
         {
             var customer = new Customer(Guid.NewGuid(), name, email, birthday, password, role);
+            ValidatCustomer(customer);
+            if (ValidationErrors.Any())
+            {
+                Console.WriteLine("Customer data is not valid:");
+                foreach (var error in ValidationErrors)
+                {
+                    Console.WriteLine(error);
+                }
+                Console.WriteLine("Please try again!");
+            }
+            else
+            {
+                Add(customer);
+            }
 
-           Add(customer);
+        }
+
+        public void ValidatCustomer(Customer customer)
+        {
+            ValidationErrors.Clear();
+            CustomerExist(customer);
+            //TODO validate password 
+            //TODO validate email
+            //TODO validate age to be more then 18 and less then 100
+            //TODO validate role
+        }
+
+        public void CustomerExist(Customer customer)
+        {
+            if (_customerRepo.GetAll().Any(c => c.Name == customer.Name))
+            {
+                ValidationErrors.Add($"Customer with name: {customer.Name} already exist");
+            }
+            if (_customerRepo.GetAll().Any(c => c.Email == customer.Email))
+            {
+                ValidationErrors.Add($"Customer with email: {customer.Email} already exist");
+            }
         }
 
 
